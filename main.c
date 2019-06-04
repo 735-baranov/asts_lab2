dfa82e47f59578155d728cf4b9f60740fc6bfd7e
Dima Baranov <baranov19972015@gmail.com>
2019-06-04 10:43:48 +0700

    stack work modificated

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
  *
  * File              : main.c
  * Compiler          : IAR for ARM
- * Version           : 7.50.2.10505
+ * Version           : 1.0001
  * Created File      : 3.03.2019
  * Last modified     : 14.05.2019
  *
@@ -17,6 +17,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+
+#define OPERATION_MODE 1
+/*
+	1 - trans parameters with stack
+	2 - overflow stack with array
+	3 - overflow stack with recursion
+	4 - two stack pointer using 
+*/ 
 
 void init(int j[]){
   for(int i=0; i<1344; ++i){
@@ -33,29 +41,35 @@ int test(int a,int b,int c,int d,int e, int x){
 }
 
 int main() {
-  
-  //stack_params
-  //int x = test(1,2,3,4,5,6);
-  
-  //overflow_stack
-  
-  //arrays
-  //int mas[1344];
-  //init(mas);
-  
-  //rec
-  //rec(5);
-  
-  //two_stack_pointers
-  /*asm(
-      "MOVS R0, #0x2 \n"
-      "MSR CONTROL, R0 \n"
-  );
-  
-  asm(
-      "MOVS R0, #0x0 \n"
-      "MSR CONTROL, R0 \n"
-  );*/
+
+	if(OPERATION_MODE==1){
+		//stack_params
+  		int x = test(1,2,3,4,5,6);
+	}
+
+	if(OPERATION_MODE==2){
+		//arrays
+	  	int mas[1344];
+	  	init(mas);
+	}
+
+	if(OPERATION_MODE==3){
+		//rec
+  		rec(5);
+	}
+
+	if(OPERATION_MODE==4){
+		//two_stack_pointers
+		asm(
+		    "MOVS R0, #0x2 \n"
+		    "MSR CONTROL, R0 \n"
+		);
+		  
+		asm(
+		    "MOVS R0, #0x0 \n"
+		    "MSR CONTROL, R0 \n"
+		);
+	}
   
   return 0;
 }
